9445dba5590aa80963c680b97d346af13ccc6a26
Ezra Keshet <ezrakeshet@gmail.com>
2022-01-13 12:07:48 -0500

    Error when variable is last in function body

--- phosphorus/parse.py
+++ phosphorus/parse.py
@@ -98,7 +98,7 @@ class Span(list):
                 peek = self[n+1]
             
             mylog("Checking " + str(item))
-            if item.type == NAME and item.string in subs and peek is not None and peek.string != "=":
+            if item.type == NAME and item.string in subs and (peek.string != "=" if peek else True):
                 s = item.string
                 spaces = item.spacebefore
                 #item = ConstantVal(s).update(subs)                
